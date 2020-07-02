71f41fc80b88b2802381fde6882ef352b6dd9591
Davide Depau <davide@depau.eu>
2020-07-02 21:37:55 +0200

    LibreSSL v3.1.3

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -3,9 +3,9 @@
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3010200fL
+#define LIBRESSL_VERSION_NUMBER 0x3010300fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.1.2"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.1.3"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
